2dbe2173927c91abb1120cf4a6a2be47f5279311
sv0401 <sv0401@naver.com>
2021-04-05 13:43:11 +0900

    second commit

--- src/main/java/com/test/jenkins/demo/TestJenkinsApplication.java
+++ src/main/java/com/test/jenkins/demo/TestJenkinsApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestJenkinsApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TestJenkinsApplication.class, args);
+
+    
+        System.out.println("test");
     }
 
 }
